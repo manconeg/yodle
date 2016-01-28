9828d97b279a40419b66686740653b422e94e306
Giuseppe Mancone <manconeg@gmail.com>
2016-01-27 23:40:13 -0500

    use project fixed

--- cli/commands/useProject.js
+++ cli/commands/useProject.js
@@ -1,20 +1,20 @@
 module.exports = {
-	name: 'useProject',
-	usage: 'use("projectId")',
-	description: 'selects a project which contains the id, matching from the right of the complete id',
+  name: 'useProject',
+  usage: 'use("projectId")',
+  description: 'selects a project which contains the id, matching from the right of the complete id',
   alias: [
-		'useProject',
-		'project',
-		'use'
+    'useProject',
+    'project',
+    'use'
   ],
   execute: function(name) {
-	  return this.yodle.useProject(name).then(function(json) {
-	    if(json.length) {
-	      this.config.project = json[0]._id;
-	      return 'project set to ' + this.config.project;
-	    } else {
-	      return 'error - project not found';
-	    }
-	  }.bind(this));
-	}
+    return this.yodle.useProject(name).then(function(project) {
+      if(project) {
+        this.config.project = project._id;
+        return 'project set to ' + this.config.project;
+      } else {
+        return 'error - project not found';
+      }
+    }.bind(this));
+  }
 };
